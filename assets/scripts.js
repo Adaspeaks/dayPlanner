8923c9736e2bc857c32f7b1b0dac0b709b4c1148
Adaspeaks <adaspeaks@gmail.com>
2021-07-28 17:24:16 -0700

    added way to get info out of local storage

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,12 @@
 // set current hour//
 const currentHour = +moment().format("H");
-
-
+//runs through hour html elements places saved tasks. Outside of function so it runs on start up
+for (let i = 1; i < 9; i++) {
+   $("#hour="+i)
+   .find("textarea")
+   .val(localStorage.getItem("hour-"=i));
+    
+};
 //Adds Job description and time to local storage//
 $(".svBtn").on("click", function (){
     const jobTimeSlot = $(this).parent().attr("id");
