28832d8e0e8b6e4da131a284956b382f98fe0694
Adaspeaks <adaspeaks@gmail.com>
2021-07-29 21:05:10 -0700

    changed date format added check for past future

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,18 @@
 const currentHour = +moment().format("H");
 
 const currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd"))
+$("#currentDay").text(currentDay.format("dddd, LL"))
+
+$(".time-block").each(function () {
+    const hour = $(this).attr("id").split("-").pop();
+    if (currentHour == hour){
+      $(this).addClass("present")
+    }
+    else if (currentHour<hour){
+      $(this).addClass("future")
+    }
+    else {$(this).addClass("past")
+    }
+    });
+    
+ 
