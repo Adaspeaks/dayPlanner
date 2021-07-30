5150b68ae5f3ada2b4d5976fb63c827ced1a805a
Adaspeaks <adaspeaks@gmail.com>
2021-07-29 21:14:48 -0700

    Added abllity to add messages to local storage

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,21 @@
 const currentHour = +moment().format("H");
 
 const currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, LL"))
+$("#currentDay").text(currentDay.format("dddd, LL"));
 
 $(".time-block").each(function () {
-    const hour = $(this).attr("id").split("-").pop();
-    if (currentHour == hour){
-      $(this).addClass("present")
-    }
-    else if (currentHour<hour){
-      $(this).addClass("future")
-    }
-    else {$(this).addClass("past")
-    }
-    });
-    
- 
+  const hour = $(this).attr("id").split("-").pop();
+  if (currentHour == hour) {
+    $(this).addClass("present");
+  } else if (currentHour < hour) {
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("past");
+  }
+});
+
+$(".svBtn").on("click", function () {
+  let jobTime = $(this).parent().attr("id");
+  let JobDesc = $(this).siblings(".description").val().trim();
+  localStorage.setItem(jobTime, JobDesc);
+});
